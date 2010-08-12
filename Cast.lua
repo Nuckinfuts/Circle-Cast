612888fd614602759fcfddc164208f76b90daa08
Greg Flynn <gflynn1@binghamton.edu>
2010-08-12 16:23:57 -0400

    this isn't CircleCast2 anymore! also fixed the cross-session variables

--- Cast.lua
+++ Cast.lua
@@ -1,4 +1,4 @@
-Circle_Cast2_Default = {
+Circle_Cast_Default = {
 	["blizz"] = false,
 	["Player_Ring"] = {
 		["r"] = 0.0,    --Red
@@ -85,14 +85,14 @@ Circle_Cast2_Default = {
 			["y"] = 0,
 			},
 		},
+	["Pet_Ring"] = {
+	    
+	    },
 };
-
-CIRCLE_CAST_TOC = 30300;
-CIRCLE_CAST_VER = "v"..CIRCLE_CAST_TOC.."-3.0 Alpha";
 
 local _G = _G;
 local loaded = nil;
-local CC2_parts = {
+local CC_parts = {
 	[1] = "_Part1",
 	[2] = "_Part2",
 	[3] = "_Part3",
@@ -102,14 +102,15 @@ local CC2_parts = {
 	[7] = "_Blue",
 }
 
-function Circle_Cast2_CreateFrames()
-	Circle_Cast2_SetupRing("Player_Ring");
-	Circle_Cast2_SetupRing("Target_Ring");
+function Circle_Cast_CreateFrames()
+	Circle_Cast_SetupRing("Player_Ring")
+	Circle_Cast_SetupRing("Target_Ring")
+	--Circle_Cast_SetupRing("Pet_Ring")
 end
 
-function Circle_Cast2_SetupRing(name)
-	local opt = CC2Options[name];
-	local ring = CreateFrame("Frame", name, _G[opt["p"]], "CastRingTemplate");
+function Circle_Cast_SetupRing(name)
+	local opt = CircleCast_Global[name];
+	local ring = CreateFrame("Frame", name, _G[opt["p"]], "CastRingTemplate")
 
 	--fix parts
 	_G[name.."_Part2"]:SetTexCoord(0, 1, 1, 1, 0, 0, 1, 0);
@@ -120,7 +121,7 @@ function Circle_Cast2_SetupRing(name)
 	if opt["Ping"] then
 		local ping = CreateFrame("Frame", name.."_Ping", ring, "CastRingTemplate_Simple");
 		for i = 1, 7 do
-			_G[name.."_Ping"..CC2_parts[i]]:SetVertexColor(Circle_Cast2_Get_Color(name, "Ping"));
+			_G[name.."_Ping"..CC_parts[i]]:SetVertexColor(Circle_Cast_GetColor(name, "Ping"));
 		end
 		_G[name.."_Ping_Part2"]:SetTexCoord(0, 1, 1, 1, 0, 0, 1, 0);
 		_G[name.."_Ping_Part3"]:SetTexCoord(1, 1, 1, 0, 0, 1, 0, 0);
@@ -143,7 +144,7 @@ function Circle_Cast2_SetupRing(name)
 		local timer = ring:CreateFontString(name.."_Timer", "ARTWORK", "GameFontNormal");
 		timer:SetWidth(opt["Timer"]["s"] * opt["Timer"]["w"]);
 		timer:SetHeight(opt["Timer"]["s"] * opt["Timer"]["h"]);
-		timer:SetVertexColor(Circle_Cast2_Get_Color(name, "Timer"));
+		timer:SetVertexColor(Circle_Cast_GetColor(name, "Timer"));
 		timer:SetFont("Fonts\\FRIZQT__.TTF", 11 * opt["Timer"]["s"], "OUTLINE")
 		timer:SetPoint(opt["Timer"]["orientA"], _G[opt["Timer"]["frame"]], opt["Timer"]["orientB"], opt["Timer"]["x"], opt["Timer"]["y"]);
 	end
@@ -153,14 +154,14 @@ function Circle_Cast2_SetupRing(name)
 		local Text = ring:CreateFontString(name.."_Text", "ARTWORK", "GameFontNormal");
 		Text:SetWidth(opt["Text"]["s"] * opt["Text"]["w"]);
 		Text:SetHeight(opt["Text"]["s"] * opt["Text"]["h"]);
-		Text:SetVertexColor(Circle_Cast2_Get_Color(name, "Text"));
+		Text:SetVertexColor(Circle_Cast_GetColor(name, "Text"));
 		Text:SetFont("Fonts\\FRIZQT__.TTF", 11 * opt["Text"]["s"], "OUTLINE")
 		Text:SetPoint(opt["Text"]["orientA"], _G[opt["Text"]["frame"]], opt["Text"]["orientB"], opt["Text"]["x"], opt["Text"]["y"]);
 	end
 	
 	--Color
 	for i = 1, 7 do
-		_G[name..CC2_parts[i]]:SetVertexColor(Circle_Cast2_Get_Color(name));
+		_G[name..CC_parts[i]]:SetVertexColor(Circle_Cast_GetColor(name));
 	end
 	
 	--Scale
@@ -171,25 +172,25 @@ function Circle_Cast2_SetupRing(name)
 	_G[name]:Hide();
 end
 
-function Circle_Cast2_Get_Color(index, index2)
+function Circle_Cast_GetColor(index, index2)
 	local r, g, b, a;
 	
 	if index2 then
-		r = CC2Options[index][index2].r;
-		g = CC2Options[index][index2].g;
-		b = CC2Options[index][index2].b;
-		a = CC2Options[index][index2].a;
+		r = CircleCast_Global[index][index2].r;
+		g = CircleCast_Global[index][index2].g;
+		b = CircleCast_Global[index][index2].b;
+		a = CircleCast_Global[index][index2].a;
 	else
-		r = CC2Options[index].r;
-		g = CC2Options[index].g;
-		b = CC2Options[index].b;
-		a = CC2Options[index].a;
+		r = CircleCast_Global[index].r;
+		g = CircleCast_Global[index].g;
+		b = CircleCast_Global[index].b;
+		a = CircleCast_Global[index].a;
 	end
 
 	return r, g, b, a;
 end
 
-function Circle_Cast2_OnLoad(self, unit, showTradeSkills)
+function Circle_Cast_OnLoad(self, unit, showTradeSkills)
 	self:RegisterEvent("UNIT_SPELLCAST_START");
 	self:RegisterEvent("UNIT_SPELLCAST_STOP");
 	self:RegisterEvent("UNIT_SPELLCAST_FAILED");
@@ -200,46 +201,32 @@ function Circle_Cast2_OnLoad(self, unit, showTradeSkills)
 	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
 	self:RegisterEvent("UNIT_TARGET");
 	self:RegisterEvent("PLAYER_ENTERING_WORLD");
+	self:RegisterEvent("ADDON_LOADED")
 
 	self.casting = nil;
 	self.channeling = nil;
 	self.targetCasting = nil;
 	self.targetChanneling = nil;
 	
-	if CC2Options == nil then
-		CC2Options = Circle_Cast2_Default;
-	end
+	SLASH_CCSLASH1, SLASH_CCSLASH2 = '/cc', '/circlecast';
 	
-	SLASH_CC2SLASH1, SLASH_CC2SLASH2 = '/cc2', '/circlecast';
-
-	if not CC2Options["blizz"] then
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_START");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_STOP");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_FAILED");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_DELAYED");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE");
-		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
-		CastingBarFrame:Hide();
-	end
-	
-	Circle_Cast2_CreateFrames();
+	--Old frame creation place
+	--Circle_Cast_CreateFrames();
 end
 
-function cc2SlashVar(var)
+function ccSlashVar(var)
 	return "|cff03C03C"..var.."|r";
 end
 
-function cc2SlashStatic(static)
+function ccSlashStatic(static)
 	return "|cffBCD4E6"..static.."|r";
 end
 
-function cc2SlashTitle(title)
+function ccSlashTitle(title)
 	return "|cffFF1111"..title.."|r";
 end
 
-function cc2SlashSplit(str)
+function ccSlashSplit(str)
 	local ret = {}
 	local c = 0;
 	for w in string.gmatch(str, " ") do
@@ -249,25 +236,25 @@ function cc2SlashSplit(str)
 	return ret
 end
 
-function SlashCmdList.CC2SLASH(msg, editbox)
+function SlashCmdList.CCSLASH(msg, editbox)
 	if msg == "help" or msg == "" then
-		print("### "..cc2SlashTitle("CC2 Configuration Help").." ###");
+		print("### "..ccSlashTitle("Circle-Cast Configuration Help").." ###");
 		print(" - Commands: (all parameters and commands must be lowercase!)");
-		print(cc2SlashStatic("/cc2 shift").." -- shift the rings position ("..cc2SlashStatic("/cc2 shift help").." for more info)");
-		print(cc2SlashStatic("/cc2 color").." -- select new colors for the rings("..cc2SlashStatic("/cc2 color help").." for more info)")
-		print(cc2SlashStatic("/cc2 reset").." -- restore default position and colors")
+		print(ccSlashStatic("/cc shift").." -- shift the rings position ("..ccSlashStatic("/cc shift help").." for more info)");
+		print(ccSlashStatic("/cc color").." -- select new colors for the rings("..ccSlashStatic("/cc color help").." for more info)")
+		print(ccSlashStatic("/cc reset").." -- restore default position and colors")
 	elseif msg == "shift help" or msg == "shift"	then
-		print("### "..cc2SlashTitle("CC2 Shift Help").." ###");
-		print(cc2SlashStatic("/CC2 SHIFT").." "..cc2SlashVar("DIRECTION OBJECT PIXELS"));
-		print(cc2SlashVar("DIRECTION").." must be up, down, left or right")
-		print(cc2SlashVar("OBJECT").." must be player or target");
-		print(cc2SlashVar("PIXELS").." must be an integer");
+		print("### "..ccSlashTitle("Circle-Cast Shift Help").." ###");
+		print(ccSlashStatic("/CC SHIFT").." "..ccSlashVar("DIRECTION OBJECT PIXELS"));
+		print(ccSlashVar("DIRECTION").." must be up, down, left or right")
+		print(ccSlashVar("OBJECT").." must be player or target");
+		print(ccSlashVar("PIXELS").." must be an integer");
 	elseif msg == "color help" or msg == "color" then
-		print("### "..cc2SlashTitle("CC2 Color Help").." ###");
-		print(cc2SlashStatic("/CC2 COLOR").." "..cc2SlashVar("OBJECT RED GREEN BLUE ALPHA"));
-		print(cc2SlashVar("OBJECT").." must be player, target, or ping");
-		print(cc2SlashVar("RED").."/"..cc2SlashVar("GREEN").."/"..cc2SlashVar("BLUE").."/"..cc2SlashVar("ALPHA").." must all be a number between or equal to 0 and 255");
-		print(cc2SlashVar("ALPHA").." is the level of transparency, 0 being completely clear and 255 being completely solid");
+		print("### "..ccSlashTitle("Cicle-Cast Color Help").." ###");
+		print(ccSlashStatic("/CC COLOR").." "..ccSlashVar("OBJECT RED GREEN BLUE ALPHA"));
+		print(ccSlashVar("OBJECT").." must be player, target, or ping");
+		print(ccSlashVar("RED").."/"..ccSlashVar("GREEN").."/"..ccSlashVar("BLUE").."/"..ccSlashVar("ALPHA").." must all be a number between or equal to 0 and 255");
+		print(ccSlashVar("ALPHA").." is the level of transparency, 0 being completely clear and 255 being completely solid");
 	elseif msg:match("shift %a+ %a+ %d+") then
 		-- /CC2 SHIFT [DIRECTION] [PLAYER or TARGET] [PIXELS]
 		msg          = msg:sub(7);
@@ -299,10 +286,10 @@ function SlashCmdList.CC2SLASH(msg, editbox)
 		end
 			
 		obj = _G[name];
-		CC2Options[name]["x"] = CC2Options[name]["x"] + x;
-		CC2Options[name]["y"] = CC2Options[name]["y"] + y;
+		CircleCast_Global[name]["x"] = CircleCast_Global[name]["x"] + x;
+		CircleCast_Global[name]["y"] = CircleCast_Global[name]["y"] + y;
 		obj:ClearAllPoints();
-		obj:SetPoint("CENTER", _G[CC2Options[name]["p"]], "CENTER", CC2Options[name]["x"], CC2Options[name]["y"])
+		obj:SetPoint("CENTER", _G[CircleCast_Global[name]["p"]], "CENTER", CircleCast_Global[name]["x"], CircleCast_Global[name]["y"])
 	elseif msg:match("color %a+ %d+ %d+ %d+ %d+") then
 		-- /CC2 COLOR [PLAYER or TARGET or PING] [RED] [GREEN] [BLUE] [ALPHA]
 		msg       = msg:sub(7);
@@ -325,39 +312,63 @@ function SlashCmdList.CC2SLASH(msg, editbox)
 		local objects = {"Player_Ring", "Target_Ring"};
 		
 		for obj in objects do
-			CC2Options[obj]["x"] = Circle_Cast2_Default[obj]["x"];
-			CC2Options[obj]["y"] = Circle_Cast2_Default[obj]["y"];
-			CC2Options[obj]["s"] = Circle_Cast2_Default[obj]["s"];
+			CircleCast_Global[obj]["x"] = Circle_Cast2_Default[obj]["x"];
+			CircleCast_Global[obj]["y"] = Circle_Cast2_Default[obj]["y"];
+			CircleCast_Global[obj]["s"] = Circle_Cast2_Default[obj]["s"];
 			_G[obj]:ClearAllPoints();
-			_G[obj]:SetPoint("CENTER", _G[CC2Options[obj]["p"]], "CENTER", CC2Options[obj]["x"], CC2Options[obj]["y"])
+			_G[obj]:SetPoint("CENTER", _G[CircleCast_Global[obj]["p"]], "CENTER", CircleCast_Global[obj]["x"], CircleCast_Global[obj]["y"])
 		end
 	else
-		print("Invalid parameter to CC2");
+		print("Invalid parameter to Circle-Cast");
 	end
 end
 
-function Circle_Cast2_OnEvent(self, event, unit)
-	if event == "UNIT_SPELLCAST_START" then
+function ccprint(msg)
+    print("<CircleCast> "..msg)
+end
+
+function Circle_Cast_OnEvent(self, event, unit)
+    --ccprint(event.." Fired "..unit)
+    if event == "ADDON_LOADED" and unit == "Circle-Cast" then
+        if CircleCast_Global == nil then
+            ccprint("Defaults Loaded")
+    		CircleCast_Global = Circle_Cast_Default
+    	end
+    	
+    	if not CircleCast_Global["blizz"] then
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_START");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_STOP");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_FAILED");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_DELAYED");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE");
+    		CastingBarFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
+    		CastingBarFrame:Hide();
+    	end
+    	
+    	Circle_Cast_CreateFrames()
+	elseif event == "UNIT_SPELLCAST_START" then
 		local _, _, text, icon, startTime, endTime, _, notInterruptible = UnitCastingInfo(unit); 
 		if unit == "player" and not self.casting and not self.channeling and startTime then
-			Circle_Cast2_SpellCast_Start(self, startTime, text, endTime - startTime, icon);
+			Circle_Cast_SpellCast_Start(self, startTime, text, endTime - startTime, icon);
 		elseif unit == "target" and startTime then
-			Circle_Cast2_SpellCast_TargetStart(self, startTime, text, endTime - startTime, icon, notInteruptable);
+			Circle_Cast_SpellCast_TargetStart(self, startTime, text, endTime - startTime, icon, notInteruptable);
 		end
 	elseif event == "UNIT_SPELLCAST_STOP" or event == "UNIT_SPELLCAST_INTERRUPTED" or event == "UNIT_SPELLCAST_CHANNEL_STOP" then
 		if unit == "player" then
-			Circle_Cast2_Reset(self);
+			Circle_Cast_Reset(self);
 		elseif unit == "target" then
-			Circle_Cast2_Reset_Target(self);
+			Circle_Cast_Reset_Target(self);
 		end
 	--let's save the cheerleader...
 	elseif event == "UNIT_TARGET" and unit == "player" then
-		Circle_Cast2_Reset_Target(self);
+		Circle_Cast_Reset_Target(self);
 		
 		if UnitExists("target") then
 			local _, _, text, icon, startTime, endTime = UnitCastingInfo("target");
 			if startTime then
-				Circle_Cast2_SpellCast_TargetStart(self, startTime, text, endTime - startTime, icon);
+				Circle_Cast_SpellCast_TargetStart(self, startTime, text, endTime - startTime, icon);
 			end
 		end
 	elseif event == "UNIT_SPELLCAST_DELAYED" then
@@ -388,19 +399,19 @@ function Circle_Cast2_OnEvent(self, event, unit)
 	end
 end
 
-function Circle_Cast2_Reset(self)
+function Circle_Cast_Reset(self)
 	self.casting = nil;
 	self.channeling = nil;
 	_G["Player_Ring"]:Hide();
 end
 
-function Circle_Cast2_Reset_Target(self)
+function Circle_Cast_Reset_Target(self)
 	self.casting_target = nil;
 	self.channeling_target = nil;
 	_G["Target_Ring"]:Hide();
 end
 
-function Circle_Cast2_OnUpdate(self, elapsed)
+function Circle_Cast_OnUpdate(self, elapsed)
 	if self.casting or self.channeling then
 		local time = GetTime();
 		if (time > self.maxValue) then
@@ -410,9 +421,9 @@ function Circle_Cast2_OnUpdate(self, elapsed)
 		local v = (time - self.startTime) / (self.maxValue - self.startTime) * 100;
 		_G["Player_Ring_Timer"]:SetText(string.format("(%.1f)", timeLeft));
 		if self.casting and not self.channeling then
-			Circle_Cast2_SetBarHeight(_G["Player_Ring"], v);
+			Circle_Cast_SetBarHeight(_G["Player_Ring"], v);
 		elseif self.channeling and not self.casting then
-			Circle_Cast2_SetBarHeight(_G["Player_Ring"], 100-v);
+			Circle_Cast_SetBarHeight(_G["Player_Ring"], 100-v);
 		end
 	end
 
@@ -427,14 +438,14 @@ function Circle_Cast2_OnUpdate(self, elapsed)
 		_G["Target_Ring_Text"]:SetText(self.spellname_target);
 		_G["Target_Ring_Timer"]:SetText(string.format("(%.1f)", timeLeft));
 		if self.casting_target and not self.channeling_target then
-			Circle_Cast2_SetBarHeight(_G["Target_Ring"], v);
+			Circle_Cast_SetBarHeight(_G["Target_Ring"], v);
 		elseif self.channeling_target and not self.casting_target then
-			Circle_Cast2_SetBarHeight(_G["Target_Ring"], 100-v);
+			Circle_Cast_SetBarHeight(_G["Target_Ring"], 100-v);
 		end
 	end
 end
 
-function Circle_Cast2_SetBarHeight(frame, p, Rev)
+function Circle_Cast_SetBarHeight(frame, p, Rev)
 	local ring1 = _G[frame:GetName().."_Part1"];
 	local ring2 = _G[frame:GetName().."_Part2"];
 	local ring3 = _G[frame:GetName().."_Part3"];
@@ -615,7 +626,7 @@ function Circle_Cast2_SetBarHeight(frame, p, Rev)
 	end
 end
 
-function Circle_Cast2_SpellCast_Start(self, start, name, duration, icon)
+function Circle_Cast_SpellCast_Start(self, start, name, duration, icon)
 	self.spellname  = name;
 	self.startTime  = start /1000;
 	self.maxValue   = self.startTime + (duration / 1000);
@@ -630,10 +641,10 @@ function Circle_Cast2_SpellCast_Start(self, start, name, duration, icon)
 	
 	local _, _, lag = GetNetStats();
 	local ping = ((lag / 1000) / self.duration) * 100;
-	Circle_Cast2_SetBarHeight(_G["Player_Ring_Ping"], ping, true);
+	Circle_Cast_SetBarHeight(_G["Player_Ring_Ping"], ping, true);
 end
 
-function Circle_Cast2_SpellChannel_Start(self, start, name, duration, icon)
+function Circle_Cast_SpellChannel_Start(self, start, name, duration, icon)
 	self.spellname  = name;
 	self.startTime  = start /1000;
 	self.maxValue   = self.startTime + (duration / 1000);
@@ -648,10 +659,10 @@ function Circle_Cast2_SpellChannel_Start(self, start, name, duration, icon)
 	
 	local _, _, lag = GetNetStats();
 	local ping = ((lag / 1000) / self.duration) * 100;
-	Circle_Cast2_SetBarHeight(_G["Player_Ring_Ping"], ping);
+	Circle_Cast_SetBarHeight(_G["Player_Ring_Ping"], ping);
 end 
 
-function Circle_Cast2_SpellCast_TargetStart(self, start, name, duration, icon, notI)
+function Circle_Cast_SpellCast_TargetStart(self, start, name, duration, icon, notI)
 	self.spellname_target  = name;
 	self.startTime_target  = start /1000;
 	self.maxValue_target   = self.startTime_target + (duration / 1000);
@@ -659,12 +670,12 @@ function Circle_Cast2_SpellCast_TargetStart(self, start, name, duration, icon, n
 	self.channeling_target = nil;
 	self.duration_target   = floor(duration / 100) / 10;
 	
-	Circle_Cast2_InteruptColor(notI)
+	Circle_Cast_InteruptColor(notI)
 	
 	_G["Target_Ring"]:Show();
 end 
 
-function Circle_Cast2_SpellChannel_TargetStart(self, start, name, duration, icon, notI)
+function Circle_Cast_SpellChannel_TargetStart(self, start, name, duration, icon, notI)
 	self.spellname_target  = name;
 	self.startTime_target  = start /1000;
 	self.maxValue_target   = self.startTime_target + (duration / 1000);
@@ -672,19 +683,19 @@ function Circle_Cast2_SpellChannel_TargetStart(self, start, name, duration, icon
 	self.channeling_target = true;
 	self.duration_target   = floor(duration / 100) / 10;
 	
-	Circle_Cast2_InteruptColor(notI)
+	Circle_Cast_InteruptColor(notI)
 	
 	_G["Target_Ring"]:Show();
 end
 
-function Circle_Cast2_InteruptColor(notI)
+function Circle_Cast_InteruptColor(notI)
 	if notI then
 		for i = 1, 7 do
-			_G["Target_Ring"..CC2_parts[i]]:SetVertexColor(Circle_Cast2_Get_Color("Target_Ring", "InteruptColor"));
+			_G["Target_Ring"..CC_parts[i]]:SetVertexColor(Circle_Cast_GetColor("Target_Ring", "InteruptColor"));
 		end
 	else
 		for i = 1, 7 do
-			_G["Target_Ring"..CC2_parts[i]]:SetVertexColor(Circle_Cast2_Get_Color("Target_Ring"));
+			_G["Target_Ring"..CC_parts[i]]:SetVertexColor(Circle_Cast_GetColor("Target_Ring"));
 		end
 	end
 end
